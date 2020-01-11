44353c940fc382b723b8ba47e464cd8f53282b77
vitalets <vitalets@yandex-team.ru>
2020-01-11 17:41:21 +0300

    supoprt node esm

---
+++ src/index.mjs
@@ -0,0 +1,20 @@
+/**
+ * Exports for ESM
+ * See: https://nodejs.org/api/esm.html#esm_approach_1_use_an_es_module_wrapper
+ */
+
+import renderer from './index.js';
+export default renderer;
+export const reply = renderer.reply;
+export const text = renderer.text;
+export const tts = renderer.tts;
+export const textTts = renderer.textTts;
+export const buttons = renderer.buttons;
+export const audio = renderer.audio;
+export const effect = renderer.effect;
+export const pause = renderer.pause;
+export const br = renderer.br;
+export const userify = renderer.userify;
+export const rand = renderer.rand;
+export const configure = renderer.configure;
+export const image = renderer.image;
